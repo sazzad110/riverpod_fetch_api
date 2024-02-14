5d573f30777de8c1afd9cbc16c0bdd99f8bf9613
sazzad110 <sazzadhasan610@gmail.com>
2024-02-14 11:27:27 +0600

    screen added

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../model/user_model.dart';
+import '../provider/data_provider.dart';
+
+class HomePage extends ConsumerWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context, ref) {
+    final userData = ref.watch(userDataProvider);
+    return Scaffold(
+        appBar: AppBar(title: const Text("Riverpod Flutter")),
+        body: userData.when(
+            data: (userData) {
+              List<UserModel> userList = userData.map((e) => e).toList();
+              return ListView.builder(
+                  itemCount: userList.length,
+                  itemBuilder: (_, index) {
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 4, horizontal: 8),
+                      child: Card(
+                          color: Theme.of(context).primaryColor,
+                          child: ListTile(
+                              title: Text(
+                                '${userList[index].firstName}  ${userList[index].lastName}',
+                                style: const TextStyle(color: Colors.white),
+                              ),
+                              subtitle: Text(
+                                '${userList[index].email}',
+                                style: const TextStyle(color: Colors.white),
+                              ),
+                              leading: CircleAvatar(
+                                backgroundImage: NetworkImage(
+                                    userList[index].avatar.toString()),
+                              ))),
+                    );
+                  });
+            },
+            error: (error, s) => Text(error.toString()),
+            loading: () => const Center(
+                  child: CircularProgressIndicator(),
+                )));
+  }
+}
